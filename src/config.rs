7ec00678f7cafdaba8e1370165c6e960975e6130
Stefan Simon <stefan.j.c.simon@gmail.com>
2025-03-30 21:35:17 +0200

    [-] Remove constants

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,3 @@
-// Hardcoded routes. Should be configurable
-pub const LOGIN_ROUTE: &str = "/login";
-pub const LOGOUT_ROUTE: &str = "/logout";
-pub const MFA_ROUTE: &str = "/login/mfa";
-
 #[derive(Clone)]
 pub struct Routes {
     login: String,
