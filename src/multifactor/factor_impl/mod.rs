bd0583cc32b36a484c66a90bf1ac95895cf9b6a8
Stefan Simon <stefan.j.c.simon@gmail.com>
2025-07-19 18:04:58 +0200

    [*] Prepare cargo.toml

--- src/multifactor/factor_impl/mod.rs
+++ src/multifactor/factor_impl/mod.rs
@@ -1,6 +1,8 @@
 //! This module contains implementations of two multifactor authentication (MFA) factors
 
 #[cfg(feature = "authenticator")]
+#[cfg_attr(docsrs, doc(cfg(feature = "authenticator")))]
 pub mod authenticator;
 #[cfg(feature = "mfa_send_code")]
+#[cfg_attr(docsrs, doc(cfg(feature = "mfa_send_code")))]
 pub mod random_code_auth;
